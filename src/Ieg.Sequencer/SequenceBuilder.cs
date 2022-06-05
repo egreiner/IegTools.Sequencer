b62fd7055ef6f0a06cfc9f76ccf17941be4056a4
Ernst <ernst.greiner@ieg-tools.net>
2022-06-05 11:21:24 +0200

    inject Validator 2/2

--- src/Ieg.Sequencer/SequenceBuilder.cs
+++ src/Ieg.Sequencer/SequenceBuilder.cs
@@ -17,7 +17,14 @@ public class SequenceBuilder : ISequenceBuilder
     /// This is good for short crispy configs.
     /// </summary>
     public static ISequenceBuilder Create() =>
-        new SequenceBuilder(new SequenceConfigurationValidator());
+        Create(new SequenceConfigurationValidator());
+
+    /// <summary>
+    /// Creates a new Sequence-Builder for configuration in .NET 6 style.
+    /// This is good for short crispy configs.
+    /// </summary>
+    public static ISequenceBuilder Create(IValidator<SequenceConfiguration> validator) =>
+        new SequenceBuilder(validator);
 
     /// <summary>
     /// Configures the sequence in .NET 5 style.
@@ -27,6 +34,19 @@ public class SequenceBuilder : ISequenceBuilder
     public static ISequenceBuilder Configure(Action<ISequenceBuilder> configurationActions)
     {
         var sequenceBuilder = Create();
+        configurationActions.Invoke(sequenceBuilder);
+        return sequenceBuilder;
+    }
+
+    /// <summary>
+    /// Configures the sequence in .NET 5 style.
+    /// This is good for larger complex configs.
+    /// </summary>
+    /// <param name="validator">Custom validator</param>
+    /// <param name="configurationActions">The action.</param>
+    public static ISequenceBuilder Configure(IValidator<SequenceConfiguration> validator, Action<ISequenceBuilder> configurationActions)
+    {
+        var sequenceBuilder = Create(validator);
         configurationActions.Invoke(sequenceBuilder);
         return sequenceBuilder;
     }
