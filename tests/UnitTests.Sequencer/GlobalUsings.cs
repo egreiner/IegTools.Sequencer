ac8a524844fb9d88899d2e9724248541a117df00
egreiner <github.ieg@ieg-tools.net>
2023-12-06 17:30:16 +0100

    add tests for logging

--- tests/UnitTests.Sequencer/GlobalUsings.cs
+++ tests/UnitTests.Sequencer/GlobalUsings.cs
@@ -1,3 +1,5 @@
 global using FluentAssertions;
 global using IegTools.Sequencer;
+global using Microsoft.Extensions.Logging;
+global using NSubstitute;
 global using Xunit;
---
+++ tests/UnitTests.Sequencer/Logging/LoggingTests.cs
@@ -0,0 +1,57 @@
+namespace UnitTests.Sequencer.Logging;
+
+public class LoggingTests
+{
+    [Fact]
+    public void Should_not_send_any_log_messages()
+    {
+        var logger = Substitute.For<ILogger<LoggingTests>>();
+        _ = SequenceBuilder.Configure(config =>
+            config.SetInitialState("Off")
+                .SetLogger(logger)
+        );
+
+        logger.Received(0).Log(
+            LogLevel.Information,
+            Arg.Any<EventId>(),
+            Arg.Any<object>(),
+            Arg.Any<Exception>(),
+            Arg.Any<Func<object, Exception?, string>>());
+    }
+
+    [Fact]
+    public void Should_send_the_expected_log_message()
+    {
+        var logger = Substitute.For<ILogger<LoggingTests>>();
+        _ = SequenceBuilder.Configure(config =>
+            config.SetInitialState("Off")
+                .SetLogger(logger, "Service is enabled")
+        );
+
+        // Assert
+        logger.Received(1).Log(
+            LogLevel.Information,
+            Arg.Any<EventId>(),
+            Arg.Is<object>(o => o.ToString() == "Service is enabled"),
+            Arg.Any<Exception>(),
+            Arg.Any<Func<object, Exception?, string>>());
+    }
+
+    [Fact]
+    public void Should_send_the_expected_log_message_with_args()
+    {
+        var logger = Substitute.For<ILogger<LoggingTests>>();
+        _ = SequenceBuilder.Configure(config =>
+            config.SetInitialState("Off")
+                .SetLogger(logger, "Service is {Status}", "disabled")
+        );
+
+        // Assert
+        logger.Received(1).Log(
+            LogLevel.Information,
+            Arg.Any<EventId>(),
+            Arg.Is<object>(o => o.ToString() == "Service is disabled"),
+            Arg.Any<Exception>(),
+            Arg.Any<Func<object, Exception?, string>>());
+    }
+}
