205d406744f04bcbed127a39d4c533db39f249d3
egreiner <github.ieg@ieg-tools.net>
2023-12-09 16:46:31 +0100

    test build alpha version

--- tests/UnitTests.Sequencer/Logging/LoggingTests.cs
+++ tests/UnitTests.Sequencer/Logging/LoggingTests.cs
@@ -1,11 +1,14 @@
 namespace UnitTests.Sequencer.Logging;
 
+using NSubstitute.Extensions;
+
 public class LoggingTests
 {
     [Fact]
     public void Should_not_send_any_log_messages()
     {
         var logger = Substitute.For<ILogger<LoggingTests>>();
+
         _ = SequenceBuilder.Configure(config =>
             config.SetInitialState("Off")
                 .SetLogger(logger, new EventId(-1))
@@ -19,39 +22,72 @@ public class LoggingTests
             Arg.Any<Func<object, Exception?, string>>());
     }
 
-    // [Fact(Skip="Obsolete")]
-    // public void Should_send_the_expected_log_message()
-    // {
-    //     var logger = Substitute.For<ILogger<LoggingTests>>();
-    //     _ = SequenceBuilder.Configure(config =>
-    //         config.SetInitialState("Off")
-    //             .SetLogger(logger, "Service is enabled")
-    //     );
-    //
-    //     // Assert
-    //     logger.Received(1).Log(
-    //         LogLevel.Information,
-    //         Arg.Any<EventId>(),
-    //         Arg.Is<object>(o => o.ToString() == "Service is enabled"),
-    //         Arg.Any<Exception>(),
-    //         Arg.Any<Func<object, Exception?, string>>());
-    // }
-    //
-    // [Fact(Skip="Obsolete")]
-    // public void Should_send_the_expected_log_message_with_args()
-    // {
-    //     var logger = Substitute.For<ILogger<LoggingTests>>();
-    //     _ = SequenceBuilder.Configure(config =>
-    //         config.SetInitialState("Off")
-    //             .SetLogger(logger, "Service is {Status}", "disabled")
-    //     );
-    //
-    //     // Assert
-    //     logger.Received(1).Log(
-    //         LogLevel.Information,
-    //         Arg.Any<EventId>(),
-    //         Arg.Is<object>(o => o.ToString() == "Service is disabled"),
-    //         Arg.Any<Exception>(),
-    //         Arg.Any<Func<object, Exception?, string>>());
-    // }
+
+    [Fact]
+    public void Should_not_send_any_log_messages_LogLevel_Information()
+    {
+        var logger = Substitute.For<ILogger<LoggingTests>>();
+
+        _ = SequenceBuilder.Configure(config =>
+            config.SetInitialState("Off")
+                .SetLogger(logger, new EventId(-1, "Seq 1"))
+        );
+
+        logger.Received(0).Log(
+            LogLevel.Information,
+            Arg.Any<EventId>(),
+            Arg.Any<object>(),
+            Arg.Any<Exception>(),
+            Arg.Any<Func<object, Exception?, string>>());
+    }
+
+    [Fact]
+    public void Should_not_send_any_log_messages_LogLevel_Debug()
+    {
+        var logger = Substitute.For<ILogger<LoggingTests>>();
+
+        _ = SequenceBuilder.Configure(config =>
+            config.SetInitialState("Off")
+                .SetLogger(logger, new EventId(-1, "Seq 1"), LogLevel.Debug)
+        );
+
+        logger.Received(0).Log(
+            LogLevel.Information,
+            Arg.Any<EventId>(),
+            Arg.Any<object>(),
+            Arg.Any<Exception>(),
+            Arg.Any<Func<object, Exception?, string>>());
+    }
+
+    [Fact]
+    public void Should_send_log_messages_force_state()
+    {
+        // Test
+        Assert.False(true);
+
+        // using ILoggerFactory factory = LoggerFactory.Create(builder => builder.ClearProviders());
+
+        var logger  = Substitute.For<ILogger<LoggingTests>>();
+
+        var builder = SequenceBuilder.Configure(config =>
+            {
+                config.SetInitialState("Off");
+                config.SetLogger(logger, new EventId(-1, "Seq 1"), LogLevel.Warning);
+                config.DisableValidation();
+
+                config.AddForceState("Test", () => true);
+            }
+        );
+
+        var sequence = builder.Build();
+        sequence.Run();
+
+        logger.Received(1).Log(
+            LogLevel.Information,
+            Arg.Any<EventId>(),
+            Arg.Any<object>(),
+            Arg.Any<Exception>(),
+            Arg.Any<Func<object, Exception?, string>>());
+    }
+
 }
