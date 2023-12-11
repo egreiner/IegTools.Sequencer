c1459038a53933c57ee8e224ef3a4ed544d01bc4
egreiner <github.ieg@ieg-tools.net>
2023-12-11 13:40:23 +0100

    wip LoggingTests

--- tests/UnitTests.Sequencer/Logging/LoggingTests.cs
+++ tests/UnitTests.Sequencer/Logging/LoggingTests.cs
@@ -1,7 +1,16 @@
 namespace UnitTests.Sequencer.Logging;
 
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute.Extensions;
+using Xunit.Abstractions;
+
 public class LoggingTests
 {
+    private ITestOutputHelper _output;
+
+    public LoggingTests(ITestOutputHelper output) =>
+        _output = output;
+
     [Fact]
     public void Should_not_send_any_log_messages()
     {
@@ -13,7 +22,37 @@ public class LoggingTests
         );
 
         logger.Received(0).Log(
-            LogLevel.Information,
+            LogLevel.Debug,
+            Arg.Any<EventId>(),
+            Arg.Any<object>(),
+            Arg.Any<Exception>(),
+            Arg.Any<Func<object, Exception?, string>>());
+    }
+
+    [Fact(Skip="wip")]
+    public void Should_send_log_messages_with_Description()
+    {
+        // var serviceProvider = new ServiceCollection()
+        //     .AddLogging(builder =>
+        //     {
+        //         builder.SetMinimumLevel(LogLevel.Debug);
+        //     })
+        //     .BuildServiceProvider();
+
+        var logger = Substitute.For<ILogger<LoggingTests>>();
+
+        var builder = SequenceBuilder.Configure(config =>
+            config.SetInitialState(TestEnum.InitialState)
+                  .ActivateDebugLogging(logger, new EventId(-1, "Test EventId"))
+                  .DisableValidation()
+                  .AddTransition("Test description", TestEnum.State1, TestEnum.State2, () => true)
+        );
+
+        var sequence = builder.Build();
+        sequence.Run();
+
+        logger.Received(1).Log(
+            LogLevel.Debug,
             Arg.Any<EventId>(),
             Arg.Any<object>(),
             Arg.Any<Exception>(),
---
+++ tests/UnitTests.Sequencer/Logging/TestEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace UnitTests.Sequencer.Logging;
+
+public enum TestEnum
+{
+    InitialState,
+    State1,
+    State2,
+    State3,
+    StateX,
+    NotExisting,
+    Unknown,
+    Force
+}
