a2cdc2142f43fe113a1300b6125f79ab5ea04806
egreiner <github.ieg@ieg-tools.net>
2023-12-24 12:42:15 +0100

    extend SimpleChangeDetectorTests.cs

--- tests/UnitTests.Sequencer/Core/SimpleChangeDetectorTests.cs
+++ tests/UnitTests.Sequencer/Core/SimpleChangeDetectorTests.cs
@@ -20,7 +20,7 @@ public class SimpleChangeDetectorTests
 
     [InlineData(null, "2", true)]
     [InlineData(null, 2, true)]
-    public void Expect_Event_raised_on_change_value<T>(T inputStart, T inputChange, bool expectRaised)
+    public void Test_OnChange_Event_raised_on_change_value<T>(T inputStart, T inputChange, bool expectRaised)
     {
         var sut = new SimpleChangeDetector<T>("UnitTest");
         T value = inputStart;
@@ -45,7 +45,7 @@ public class SimpleChangeDetectorTests
 
     [InlineData(null, "2")]
     [InlineData(null, 2)]
-    public void Expect_disable_detection<T>(T inputStart, T inputChange)
+    public void Test_OnChange_Event_disable_detection<T>(T inputStart, T inputChange)
     {
         var sut   = new SimpleChangeDetector<T>("UnitTest");
         T   value = inputStart;
@@ -53,6 +53,85 @@ public class SimpleChangeDetectorTests
 
         value = inputChange;
         sut.Detect(false);
+
+        _eventValueChangedRaised.Should().Be(false);
+    }
+
+
+    [Theory]
+    [InlineData(null, null)]
+    [InlineData(0, 0)]
+    [InlineData(10, 10)]
+    [InlineData(1, 2)]
+    [InlineData(0.1, 0.1)]
+    [InlineData(1.1, 2.1)]
+    [InlineData("1", "1")]
+    [InlineData("1", "2")]
+
+    [InlineData(null, "2")]
+    [InlineData(null, 2)]
+    public void Test_OnChange_Event_SuspendAction<T>(T inputStart, T inputChange)
+    {
+        var sut   = new SimpleChangeDetector<T>("UnitTest");
+        T   value = inputStart;
+        sut.OnChange(() => value, () => { _eventValueChangedRaised = true; } );
+
+        value = inputChange;
+        sut.SuspendAction();
+        sut.Detect();
+
+        _eventValueChangedRaised.Should().Be(false);
+    }
+
+
+    [Theory]
+    [InlineData(null, null, false)]
+    [InlineData(0, 0, false)]
+    [InlineData(10, 10, false)]
+    [InlineData(1, 2, true)]
+    [InlineData(0.1, 0.1, false)]
+    [InlineData(1.1, 2.1, true)]
+    [InlineData("1", "1", false)]
+    [InlineData("1", "2", true)]
+
+    [InlineData(null, "2", true)]
+    [InlineData(null, 2, true)]
+    public void Test_OnChange_Event_ResumeAction<T>(T inputStart, T inputChange, bool expectRaised)
+    {
+        var sut   = new SimpleChangeDetector<T>("UnitTest");
+        T   value = inputStart;
+        sut.OnChange(() => value, () => { _eventValueChangedRaised = true; } );
+        sut.SuspendAction();
+
+        value = inputChange;
+        sut.ResumeAction();
+        sut.Detect();
+
+        _eventValueChangedRaised.Should().Be(expectRaised);
+    }
+
+
+    [Theory]
+    [InlineData(null, null)]
+    [InlineData(0, 0)]
+    [InlineData(10, 10)]
+    [InlineData(1, 2)]
+    [InlineData(0.1, 0.1)]
+    [InlineData(1.1, 2.1)]
+    [InlineData("1", "1")]
+    [InlineData("1", "2")]
+
+    [InlineData(null, "2")]
+    [InlineData(null, 2)]
+    public void Test_OnChange_SetValue<T>(T inputStart, T inputChange)
+    {
+        var sut   = new SimpleChangeDetector<T>("UnitTest");
+        T   value = inputStart;
+        sut.OnChange(() => value, () => { _eventValueChangedRaised = true; } );
+
+        value = inputChange;
+        sut.SetValue(inputChange);
+        sut.Detect();
 
         _eventValueChangedRaised.Should().Be(false);
     }
