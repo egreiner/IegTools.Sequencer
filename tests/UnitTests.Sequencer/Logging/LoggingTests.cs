20755297312d2d173b8476300fc757df36be5ee9
egreiner <github.ieg@ieg-tools.net>
2023-12-26 11:16:49 +0100

    add tests for logging

--- tests/UnitTests.Sequencer/Logging/LoggingTests.cs
+++ tests/UnitTests.Sequencer/Logging/LoggingTests.cs
@@ -1,16 +1,11 @@
 namespace UnitTests.Sequencer.Logging;
 
-using Microsoft.Extensions.DependencyInjection;
-using NSubstitute.Extensions;
-using Xunit.Abstractions;
-
 public class LoggingTests
 {
     [Fact]
     public void Should_not_send_any_log_messages()
     {
         var logger = Substitute.For<ILogger<LoggingTests>>();
-
         _ = SequenceBuilder.Configure(config =>
             config.SetInitialState("Off")
                 .ActivateDebugLogging(logger, new EventId(-1, "Test EventId"))
@@ -24,20 +19,17 @@ public class LoggingTests
             Arg.Any<Func<object, Exception?, string>>());
     }
 
-    // [Fact(Skip="wip")]
     [Fact]
-    public void Should_send_log_messages_with_Description()
+    public void Should_send_log_messages_for_Transition_with_Description()
     {
         var logger = Substitute.For<ILogger<LoggingTests>>();
-
-        var builder = SequenceBuilder.Configure(config =>
+        var sequence = SequenceBuilder.Configure(config =>
             config.SetInitialState(TestEnum.InitialState)
-                  .ActivateDebugLogging(logger, new EventId(-1, "Test EventId"))
-                  .DisableValidation()
-                  .AddTransition("Test description", TestEnum.InitialState, TestEnum.State2, () => true)
-        );
+                .ActivateDebugLogging(logger, new EventId(-1, "Test EventId"))
+                .DisableValidation()
+                .AddTransition("Test description", TestEnum.InitialState, TestEnum.State2, () => true)
+        ).Build();
 
-        var sequence = builder.Build();
         sequence.Run();
 
         logger.Received(1).Log(
@@ -48,32 +40,86 @@ public class LoggingTests
             Arg.Any<Func<object, Exception?, string>>());
     }
 
-
-    [Fact(Skip="wip")]
-    public void Should_send_log_messages_force_state()
+    [Fact]
+    public void Should_send_log_messages_for_ForceState_with_Description()
     {
-        // Test
-        // Assert.False(true);
+        var logger = Substitute.For<ILogger<LoggingTests>>();
+        var sequence = SequenceBuilder.Configure(config =>
+            config.SetInitialState(TestEnum.InitialState)
+                .ActivateDebugLogging(logger, new EventId(-1, "Test EventId"))
+                .DisableValidation()
+                .AddForceState("Test description", TestEnum.State2, () => true)
+        ).Build();
 
-        // using ILoggerFactory factory = LoggerFactory.Create(builder => builder.ClearProviders());
-
-        var logger  = Substitute.For<ILogger<LoggingTests>>();
-
-        var builder = SequenceBuilder.Configure(config =>
-            {
-                config.SetInitialState("Off");
-                config.ActivateDebugLogging(logger, new EventId(-1, "Seq 1"));
-                config.DisableValidation();
-
-                config.AddForceState("Test", () => true);
-            }
-        );
-
-        var sequence = builder.Build();
         sequence.Run();
 
         logger.Received(1).Log(
-            LogLevel.Information,
+            LogLevel.Debug,
+            Arg.Any<EventId>(),
+            Arg.Any<object>(),
+            Arg.Any<Exception>(),
+            Arg.Any<Func<object, Exception?, string>>());
+    }
+
+    [Fact]
+    public void Should_send_log_messages_for_ContainsTransition_with_Description()
+    {
+        var logger = Substitute.For<ILogger<LoggingTests>>();
+        var sequence = SequenceBuilder.Configure(config =>
+            config.SetInitialState(TestEnum.InitialState)
+                .ActivateDebugLogging(logger, new EventId(-1, "Test EventId"))
+                .DisableValidation()
+                .AddContainsTransition("Test description", "Init", TestEnum.State2, () => true)
+        ).Build();
+
+        sequence.Run();
+
+        logger.Received(1).Log(
+            LogLevel.Debug,
+            Arg.Any<EventId>(),
+            Arg.Any<object>(),
+            Arg.Any<Exception>(),
+            Arg.Any<Func<object, Exception?, string>>());
+    }
+
+    [Fact]
+    public void Should_send_log_messages_for_AnyTransition_with_Description()
+    {
+        var logger = Substitute.For<ILogger<LoggingTests>>();
+        var sequence = SequenceBuilder.Configure(config =>
+            config.SetInitialState(TestEnum.InitialState)
+                .ActivateDebugLogging(logger, new EventId(-1, "Test EventId"))
+                .DisableValidation()
+                .AddAnyTransition("Test description", [TestEnum.InitialState,TestEnum.State1], TestEnum.State2, () => true)
+        ).Build();
+
+        sequence.Run();
+
+        logger.Received(1).Log(
+            LogLevel.Debug,
+            Arg.Any<EventId>(),
+            Arg.Any<object>(),
+            Arg.Any<Exception>(),
+            Arg.Any<Func<object, Exception?, string>>());
+    }
+
+    [Fact]
+    public void Should_send_log_messages_for_StateAction_with_Description()
+    {
+        var x      = 0;
+        var logger = Substitute.For<ILogger<LoggingTests>>();
+        var sequence = SequenceBuilder.Configure(config =>
+            config.SetInitialState(TestEnum.InitialState)
+                .ActivateDebugLogging(logger, new EventId(-1, "Test EventId"))
+                .DisableValidation()
+                .AddStateAction("Test description", TestEnum.InitialState, () => x = 100, () => true)
+        ).Build();
+
+        sequence.Run();
+
+        x.Should().Be(100);
+        logger.Received(1).Log(
+            LogLevel.Debug,
             Arg.Any<EventId>(),
             Arg.Any<object>(),
             Arg.Any<Exception>(),
