7fb6f864816855cad310a303a90417d4f8e5557c
egreiner <github.ieg@ieg-tools.net>
2024-09-27 16:37:15 +0200

    add StringExtensionsTests.cs

---
+++ tests/UnitTests.Sequencer/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,68 @@
+﻿namespace UnitTests.Sequencer.Extensions;
+
+public class StringExtensionsTests
+{
+    [Fact]
+    public void MatchesAny_ShouldBeTrue_WhenStringParamsContainsText()
+    {
+        var text = "text";
+
+        var actual  = text.MatchesAny("text", "text1", "text2");
+
+        actual.Should().BeTrue();
+    }
+
+    [Fact]
+    public void MatchesAny_ShouldBeFalse_WhenStringParamsDoesNotContainText()
+    {
+        var text = "not found";
+
+        var actual  = text.MatchesAny("text", "text1", "text2");
+
+        actual.Should().BeFalse();
+    }
+
+    [Fact]
+    public void MatchesAny_ShouldBeTrue_WhenStringArrayContainsText()
+    {
+        var text = "text";
+        var stringArray = new[] { "text", "text1", "text2" };
+
+        var actual  = text.MatchesAny(stringArray);
+
+        actual.Should().BeTrue();
+    }
+
+    [Fact]
+    public void MatchesAny_ShouldBeFalse_WhenStringArrayDoesNotContainText()
+    {
+        var text = "not found";
+        var stringArray = new[] { "text", "text1", "text2" };
+
+        var actual  = text.MatchesAny(stringArray);
+
+        actual.Should().BeFalse();
+    }
+
+    [Fact]
+    public void MatchesAny_ShouldBeFalse_WhenStringArrayOrStringParamsContainsText()
+    {
+        var text = "text3";
+        var stringArray = new[] { "text", "text1", "text2" };
+
+        var actual  = text.MatchesAny(stringArray, "text3", "text4");
+
+        actual.Should().BeTrue();
+    }
+
+    [Fact]
+    public void MatchesAny_ShouldBeFalse_WhenStringArrayAndStringParamsDoNotContainText()
+    {
+        var text = "not found";
+        var stringArray = new[] { "text", "text1", "text2" };
+
+        var actual  = text.MatchesAny(stringArray, "text3", "text4");
+
+        actual.Should().BeFalse();
+    }
+}
